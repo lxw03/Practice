bdcbaa032525967b5270d356bd99b898635a90b7
cw <284619466@qq.com>
2017-02-06 22:27:06 +0800

    tablyout + viewpager

--- app/src/main/java/com/example/cw/practice/ui/fragment/MainNews.java
+++ app/src/main/java/com/example/cw/practice/ui/fragment/MainNews.java
@@ -2,7 +2,10 @@ package com.example.cw.practice.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +18,41 @@ import com.example.cw.practice.R;
 
 public class MainNews extends Fragment {
 
+    private String[] mTabs = {"tab1", "tab2", "tab3", "tab4", "tab5"};
+    private Fragment[] mFragments = {new MainMeizi(), new MainVideo(), new MainMeizi(), new MainVideo(), new MainMe()};
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.news_main, container, false);
+        initTabs(view);
         return view;
+    }
+
+    private void initTabs(View view) {
+        mViewPager = (ViewPager) view.findViewById(R.id.vp_news);
+        mTabLayout = (TabLayout) view.findViewById(R.id.tablayout_news);
+        for (int i=0; i<mTabs.length; i++){
+            mTabLayout.addTab(mTabLayout.newTab().setText(mTabs[i]));
+        }
+        mTabLayout.setupWithViewPager(mViewPager, true);
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(getActivity().getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments[position];
+            }
+
+            @Override
+            public int getCount() {
+                return mTabs.length;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mTabs[position];
+            }
+        });
     }
 }
