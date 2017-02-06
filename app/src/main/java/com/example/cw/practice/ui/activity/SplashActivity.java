75b7bb6d7f8063ea860228c71d1d11f60036abeb
陈伟 <wchen@yinnut.com>
2017-02-06 13:22:42 +0800

    v1.0

---
+++ app/src/main/java/com/example/cw/practice/ui/activity/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.cw.practice.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.cw.practice.R;
+
+/**
+ * Created by chenwei on 17/2/6.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    //试下lottie
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
