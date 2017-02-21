e114889dbe94305d83fe962412737f7f3bb9d0d9
cw <284619466@qq.com>
2017-02-21 19:51:11 +0800

    v1.0

--- app/src/main/java/com/example/cw/practice/api/Api.java
+++ app/src/main/java/com/example/cw/practice/api/Api.java
@@ -1,66 +1,19 @@
 package com.example.cw.practice.api;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.HttpUrl;
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 
 /**
  * Created by cw on 2017/2/18.
  */
 
 public class Api {
-    private static ApiService SERVICE;
 
-    /**
-     * 请求超时
-     */
-    private static final int DEAFULT_TIMEOUT = 10*1000;
+    //读超时长
+    public static final int READ_TIME_OUT = 7676;
+    //连接时长
+    public static final int CONNECT_TIME_OUT = 7676;
 
-    public static ApiService getDefault(){
-        if (SERVICE == null){
-            //手动创建一个http并设置超时
-            OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
-            httpClientBuilder.connectTimeout(DEAFULT_TIMEOUT, TimeUnit.MILLISECONDS);
+    public Retrofit mRetrofit;
+    public ApiService mApiService;
 
-            //对所有请求添加请求头
-            httpClientBuilder.addInterceptor(new Interceptor() {
-                @Override
-                public Response intercept(Chain chain) throws IOException {
-                    Request request = chain.request();
-
-                    HttpUrl.Builder authorizedUrlBuilder = request.url()
-                            .newBuilder()
-                            //添加统一参数，如token
-                            .addQueryParameter("key1", "value1")
-                            .addQueryParameter("key2", "value2");
-
-                    Request newRequest = request.newBuilder()
-                            //对所有请求添加请求头
-                            .header("mobileFlag", "abc").addHeader("type", "4")
-                            .method(request.method(), request.body())
-                            .url(authorizedUrlBuilder.build())
-                            .build();
-
-                    return chain.proceed(newRequest);
-                }
-            });
-
-            SERVICE = new Retrofit.Builder()
-                    .client(httpClientBuilder.build())
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                    .baseUrl(com.example.cw.practice.api.Url.BASE_URL)
-                    .build().create(ApiService.class);
-        }
-        return SERVICE;
-    }
 }
--- app/src/main/java/com/example/cw/practice/api/ApiService.java
+++ app/src/main/java/com/example/cw/practice/api/ApiService.java
@@ -1,11 +1,15 @@
 package com.example.cw.practice.api;
 
 import com.example.cw.practice.bean.HttpResult;
+import com.example.cw.practice.bean.NewsSummary;
 import com.example.cw.practice.bean.Subject;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -17,4 +21,10 @@ public interface ApiService {
 
     @GET("top250")
     Observable<HttpResult<List<Subject>>> getTopMovie(@Query("start") int start, @Query("count") int count);
+
+    @GET("nc/article/{type}/{id}/{startPage}-20.html")
+    Observable<Map<String, List<NewsSummary>>> getNewsList(
+            @Header("Cache-Control") String cacheControl,
+            @Path("type") String type, @Path("id") String id,
+            @Path("startPage") int startPage);
 }
