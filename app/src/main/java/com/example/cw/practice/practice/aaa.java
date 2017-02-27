429390e6ed8e40dc73cfe5a2f7dd2fbbf9d95d67
陈伟 <wchen@yinnut.com>
2017-02-27 18:26:40 +0800

    rxjava

--- app/src/main/java/com/example/cw/practice/practice/aaa.java
+++
@@ -1,8 +0,0 @@
-package com.example.cw.practice.practice;
-
-/**
- * Created by chenwei on 17/2/21.
- */
-
-public class aaa {
-}
---
+++ app/src/main/java/com/example/cw/practice/practice/rxJava/RxJavaPractice.java
@@ -0,0 +1,21 @@
+package com.example.cw.practice.practice.rxJava;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chenwei on 17/2/27.
+ */
+
+public class RxJavaPractice extends AppCompatActivity {
+
+
+    // RxJava练习
+    // 观察者订阅被观察者
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
