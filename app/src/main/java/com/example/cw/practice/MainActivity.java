effd3b73d08301211944810c6d3716c6f49eb92c
cw <284619466@qq.com>
2017-03-05 14:51:55 +0800

    insist on

--- app/src/main/java/com/example/cw/practice/MainActivity.java
+++ app/src/main/java/com/example/cw/practice/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.example.cw.practice.ui.fragment.MainMe;
 import com.example.cw.practice.ui.fragment.MainMeizi;
+import com.example.cw.practice.ui.fragment.MainMore;
 import com.example.cw.practice.ui.fragment.MainNews;
 import com.example.cw.practice.ui.fragment.MainVideo;
 import com.flyco.tablayout.CommonTabLayout;
@@ -16,17 +17,18 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String [] mTitles = {"新闻", "妹子", "视频", "我"};
+    private String [] mTitles = {"新闻", "好友", "视频", "练习","更多"};
     private int[] mIconUnselectIds = {
-            R.mipmap.ic_home_normal,R.mipmap.ic_girl_normal,R.mipmap.ic_video_normal,R.mipmap.ic_care_normal};
+            R.mipmap.ic_home_normal,R.mipmap.ic_friend_unselected,R.mipmap.ic_video_normal,R.mipmap.ic_care_normal, R.mipmap.ic_more_normal};
     private int[] mIconSelectIds = {
-            R.mipmap.ic_home_selected,R.mipmap.ic_girl_selected, R.mipmap.ic_video_selected,R.mipmap.ic_care_selected};
+            R.mipmap.ic_home_selected,R.mipmap.ic_friend_selected, R.mipmap.ic_video_selected,R.mipmap.ic_care_selected,R.mipmap.ic_more_selected};
     private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
 
     private MainNews mainNewsFragment;
     private MainMeizi mainMeiziFragment;
     private MainVideo mainVideoFragment;
     private MainMe mainMeFragment;
+    private MainMore mainMoreFragment;
     private static int tabLayoutHeight;
 
     private CommonTabLayout commonTabLayout;
@@ -46,17 +48,20 @@ public class MainActivity extends AppCompatActivity {
             mainNewsFragment = (MainNews) getSupportFragmentManager().findFragmentByTag("mainNewsFragment");
             mainMeiziFragment = (MainMeizi) getSupportFragmentManager().findFragmentByTag("mainMeiziFragment");
             mainVideoFragment = (MainVideo) getSupportFragmentManager().findFragmentByTag("mainVideoFragment");
+            mainMoreFragment = (MainMore) getSupportFragmentManager().findFragmentByTag("mainMoreFragment");
             currentTabPosition = savedInstanceState.getInt("current_tab_position");
         }else {
             mainNewsFragment = new MainNews();
             mainMeiziFragment = new MainMeizi();
             mainVideoFragment = new MainVideo();
             mainMeFragment = new MainMe();
+            mainMoreFragment = new MainMore();
 
             transaction.add(R.id.fl_body, mainNewsFragment, "mainNewsFragment");
             transaction.add(R.id.fl_body, mainMeiziFragment, "mainMeiziFragment");
             transaction.add(R.id.fl_body, mainVideoFragment, "mainVideoFragment");
             transaction.add(R.id.fl_body, mainMeFragment, "mainMeFragment");
+            transaction.add(R.id.fl_body, mainMoreFragment, "mainMoreFragment");
         }
         transaction.commit();
         changeFragment(currentTabPosition);
@@ -110,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
                 transaction.hide(mainMeiziFragment);
                 transaction.hide(mainVideoFragment);
                 transaction.hide(mainMeFragment);
+                transaction.hide(mainMoreFragment);
                 transaction.show(mainNewsFragment);
                 transaction.commitAllowingStateLoss();
                 break;
@@ -117,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
                 transaction.hide(mainVideoFragment);
                 transaction.hide(mainMeFragment);
                 transaction.hide(mainNewsFragment);
+                transaction.hide(mainMoreFragment);
                 transaction.show(mainMeiziFragment);
                 transaction.commitAllowingStateLoss();
                 break;
@@ -124,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
                 transaction.hide(mainMeFragment);
                 transaction.hide(mainNewsFragment);
                 transaction.hide(mainMeiziFragment);
+                transaction.hide(mainMoreFragment);
                 transaction.show(mainVideoFragment);
                 transaction.commitAllowingStateLoss();
                 break;
@@ -131,7 +139,16 @@ public class MainActivity extends AppCompatActivity {
                 transaction.hide(mainNewsFragment);
                 transaction.hide(mainMeiziFragment);
                 transaction.hide(mainVideoFragment);
+                transaction.hide(mainMoreFragment);
                 transaction.show(mainMeFragment);
+                transaction.commitAllowingStateLoss();
+                break;
+            case 4:
+                transaction.hide(mainNewsFragment);
+                transaction.hide(mainMeiziFragment);
+                transaction.hide(mainVideoFragment);
+                transaction.hide(mainMeFragment);
+                transaction.show(mainMoreFragment);
                 transaction.commitAllowingStateLoss();
                 break;
             default:
