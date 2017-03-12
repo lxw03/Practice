7c4219ff09f55b822c68505693807b24b95f896f
chenwei <chenw@eengoo.com>
2017-03-12 18:34:32 +0800

    custom view

---
+++ app/src/main/java/com/example/cw/practice/practice/view/CircleView.java
@@ -0,0 +1,94 @@
+package com.example.cw.practice.practice.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.cw.practice.R;
+
+/**
+ * Created by eengoo on 17/3/12.
+ */
+
+//自定义view须知
+//    1. 让View支持wrap_content，如果不在onMeasure中对wrap_content做特殊处理，在布局中使用wrap_content时就无法达到预期的效果
+//    2. 如果有必要，让View支持padding
+//    3. 尽量不要在View中使用Handler，View内部本身就提供了post系列的方法
+//    4. View中有线程、动画需要及时停止， onDetachedFromWindow
+//    5. View带有滑动嵌套情形时，需要处理好滑动冲突
+
+public class CircleView extends View {
+    private static final int color = Color.RED;
+    private Paint mPaint;
+    private int mColor;
+
+    public CircleView(Context context) {
+        super(context);
+        initViews();
+    }
+
+    private void initViews() {
+        //在Values目录下创建自定义属性xml
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(mColor);
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initViews();
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        //获取自定义属性
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleView);
+        mColor = a.getColor(R.styleable.CircleView_circle_color, color);
+        a.recycle();
+
+        initViews();
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initViews();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+        if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(200,200);
+        }else if (widthSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(200, heightSpecSize);
+        }else if (heightSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(widthSpecSize, 200);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int paddingLeft = getPaddingLeft();
+        int paddingRight = getPaddingRight();
+        int paddingTop = getPaddingTop();
+        int paddingBottom = getPaddingBottom();
+        int width = getWidth() - paddingLeft -paddingRight;
+        int height = getHeight() - paddingTop - paddingBottom;
+        canvas.drawCircle(paddingLeft+width/2, paddingTop+height/2, Math.min(width,height), mPaint);
+    }
+
+    //注意点
+    //1. 继承子View和ViewGroup的组件，padding是默认无法生效的，需要自己处理
+//    2. 对于直接继承View的控件，如果不对wrap_content做特殊处理，那么使用wrap_content就相当于match_parent
+}
---
+++ app/src/main/java/com/example/cw/practice/practice/view/CustomView.java
@@ -0,0 +1,74 @@
+package com.example.cw.practice.practice.view;
+
+import android.content.Context;
+import android.support.annotation.IntDef;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+/**
+ * Created by eengoo on 17/3/12.
+ */
+
+//MeasureSpec = SpecMode + SpecSize 打包成一个int值来避免过多的内存对象分配
+//UNSPECIFIED AT_MOST EXACTLY
+//对于顶级的DecorView，其MeasureSpec由窗口的尺寸和自身的LayoutParams决定，对于普通View，有父容器的MeasureSpec和自身的LayoutParams决定
+
+    //onMeasure方法中拿到的测量宽高可能不是准确的，比较好的习惯是在onLayout方法中获取View的测量宽高或者最终宽高
+    //View的Measure过程和Activity的生命周期不是同步执行的，因此无法确保onResume时View已经测量完毕了，解决方法
+    // 1. onWindowFocusHasChanged   2.通过post将一个Runnable投递到消息队列的尾部，等到Looper调用此Runnable的时候，View也已经初始化好了
+    // 3.ViewTreeObserver  4. view.measure
+
+public class CustomView extends AppCompatActivity{
+
+    private View view;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = view.getMeasuredWidth();
+                int height = view.getMeasuredHeight();
+            }
+        });
+
+        ViewTreeObserver viewTreeObserver = view.getViewTreeObserver();
+        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                int width = view.getMeasuredWidth();
+                int height = view.getMeasuredHeight();
+            }
+        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        super.onWindowFocusChanged(hasWindowFocus);
+        if (hasWindowFocus){
+            int width = view.getMeasuredWidth();
+            int height = view.getMeasuredHeight();
+        }
+    }
+
+    private void viewMeasure(){
+        //match_parent  此时无法知道parentSize多大
+        //exactly
+        int widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(100, View.MeasureSpec.EXACTLY);
+        int heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(100, View.MeasureSpec.EXACTLY);
+        view.measure(widthMeasureSpec, heightMeasureSpec);
+        //wrap_content
+        //view的尺寸使用30位的二进制表示，最大为2^30-1 == (1<<30) -1
+        int widthMeasureSpec_wrap_content = View.MeasureSpec.makeMeasureSpec( (1<<30) -1, View.MeasureSpec.AT_MOST);
+        int heightMeasureSpec_wrap_content = View.MeasureSpec.makeMeasureSpec( (1<<30) -1, View.MeasureSpec.AT_MOST);
+        view.measure(widthMeasureSpec_wrap_content,heightMeasureSpec_wrap_content);
+    }
+
+
+    //layout方法确定view本身的位置，onLayout方法确定所有子View的位置
+}
