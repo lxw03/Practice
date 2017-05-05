d7639100eb89140c3fc11b08318c60edbf630967
templechen <templechen@qiyi.com>
2017-05-05 10:53:56 +0800

    fix gson convert to class

--- app/src/main/java/com/example/cw/iqiyi/model/MovieSubject.java
+++ app/src/main/java/com/example/cw/iqiyi/model/MovieSubject.java
@@ -1,75 +1,34 @@
 package com.example.cw.iqiyi.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by cw on 2017/5/4.
  */
 
-public class MovieSubject implements Parcelable{
+public class MovieSubject{
 
 //    {"ctime":"2017-05-04 16:55","title":"10岁男孩被爷爷扔路边：只因有两张试卷没做完",
 //            "description":"搜狐社会","picUrl":"http:\/\/photocdn.sohu.com\/20170504\/Img491748501_ss.jpeg",
 //            "url":"http:\/\/news.sohu.com\/20170504\/n491748755.shtml"}]}
 
+
+    public MovieSubject() {
+    }
+
+    @SerializedName("ctime")
     private String ctime;
+
+    @SerializedName("title")
     private String title;
+
+    @SerializedName("description")
     private String description;
+
+    @SerializedName("picUrl")
     private String picUrl;
+
+    @SerializedName("url")
     private String url;
 
-    protected MovieSubject(Parcel in) {
-        ctime = in.readString();
-        title = in.readString();
-        description = in.readString();
-        picUrl = in.readString();
-        url = in.readString();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(ctime);
-        dest.writeString(title);
-        dest.writeString(description);
-        dest.writeString(picUrl);
-        dest.writeString(url);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<MovieSubject> CREATOR = new Creator<MovieSubject>() {
-        @Override
-        public MovieSubject createFromParcel(Parcel in) {
-            return new MovieSubject(in);
-        }
-
-        @Override
-        public MovieSubject[] newArray(int size) {
-            return new MovieSubject[size];
-        }
-    };
-
-    public String getCtime() {
-        return ctime;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getPicUrl() {
-        return picUrl;
-    }
-
-    public String getUrl() {
-        return url;
-    }
 }
--- app/src/main/java/com/example/cw/iqiyi/model/MovieSubjects.java
+++ app/src/main/java/com/example/cw/iqiyi/model/MovieSubjects.java
@@ -1,58 +1,26 @@
 package com.example.cw.iqiyi.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
 
 /**
  * Created by cw on 2017/5/4.
  */
 
-public class MovieSubjects implements Parcelable{
+public class MovieSubjects{
 
+    public MovieSubjects() {
+    }
+
+    @SerializedName("code")
     private String code;
+
+    @SerializedName("msg")
     private String msg;
-    private MovieSubject newslist;
+
+    @SerializedName("newslist")
+    private List<MovieSubject> newslist;
 
 
-    protected MovieSubjects(Parcel in) {
-        code = in.readString();
-        msg = in.readString();
-        newslist = in.readParcelable(MovieSubject.class.getClassLoader());
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(code);
-        dest.writeString(msg);
-        dest.writeParcelable(newslist, flags);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<MovieSubjects> CREATOR = new Creator<MovieSubjects>() {
-        @Override
-        public MovieSubjects createFromParcel(Parcel in) {
-            return new MovieSubjects(in);
-        }
-
-        @Override
-        public MovieSubjects[] newArray(int size) {
-            return new MovieSubjects[size];
-        }
-    };
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public MovieSubject getNewslist() {
-        return newslist;
-    }
 }
